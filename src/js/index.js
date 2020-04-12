5c2a96013f7107c47e2619c2abf1a7185bc10f66
Vi Giang <vigiangdev@gmail.com>
2020-04-12 10:14:16 -0700

    added summary

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,109 @@
 require("@babel/polyfill");
+import { apiData } from './apiData'
+
+// DOM SELECTORS
+const elements = {
+
+    // summary
+    sumCasesTotal: document.querySelector('.cases-total'),
+    sumRecoveredTotal: document.querySelector('.recovered-total'),
+    sumDeathsTotal: document.querySelector('.deaths-total'),
+
+    sumCasesList: document.querySelector('.cases-list-body'),
+    sumRecoveredList: document.querySelector('.recovered-list-body'),
+    sumDeathsList: document.querySelector('.deaths-list-body'),
+}
 
 const state = {};
 
-const getData = async function () {
-    await fetch("https://covid-193.p.rapidapi.com/statistics", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-193.p.rapidapi.com",
-            "x-rapidapi-key": "fa2431ca2fmshfde80ab5f3da76fp11c601jsn3126398e0928"
-        }
-    })
-        .then(response => {
-            return response.json();
-        })
-        .then(result => {
-            state.results = result.response;
-        })
-        .catch(err => {
-            console.log('fetchAPI', err);
-        });
+// // CONTROLLER
+async function controller() {
+
+    // get data from API and store it in state.countryResults
+    try {
+        await getResult();
+
+        displayAllTotal();
+
+        // displayCountryResults();
+
+    } catch (err) {
+        console.log('controller', err);
+    }
 }
+
+
+// // MODEL
+async function getResult() {
+    // await fetch("https://api.covid19api.com/summary"
+    // )
+    //     .then(response => {
+    //         return response.json();
+    //     })
+    //     .then(result => {
+    //         console.log(result);
+    //         state.globalResults = result.Global;
+    //         state.countryResults = result.Countries;
+    //     })
+    //     .catch(err => {
+    //         console.log('fetchAPI', err);
+    //     });
+};
+
+
+
+// VIEW
+// ALL
+function displayAllTotal() {
+    console.log(state);
+    console.log(state.globalResults);
+
+    // elements.sumCasesTotal.innerHTML = `${state.globalResults.TotalConfirmed}`;
+    // state.countryResults.forEach(country => {
+    //     elements.sumCasesList.insertAdjacentHTML('beforeend', `
+    //     <tr>
+    //         <td>${country.Country}</td>
+    //         <td>${country.TotalConfirmed}</td>
+    //     </tr>
+    // `)
+    // })
+}
+
+
+
+
+
+
+
+
+
+// function displayCountryResults() {
+//     state.countryResults.forEach(country => {
+//         elements.resData.insertAdjacentHTML('beforeend', `
+//             <tr>
+//                 <td>${country.Country}</td>
+//                 <td>${country.NewConfirmed}</td>
+//                 <td>${country.TotalConfirmed}</td>
+//                 <td>${country.NewDeaths}</td>
+//                 <td>${country.TotalDeaths}</td>
+//                 <td>${country.NewRecovered}</td>
+//                 <td>${country.TotalRecovered}</td>
+//             </tr>
+//             `
+//         )
+//     });
+// }
+
+// function displayTotalResults() {
+//     elements.resTotal.innerHTML = 
+//     elements.resToday.innerHTML = 
+
+// }
+
+
+controller();
+
+
+
+
 
---
+++ src/js/model/Search.js
@@ -0,0 +1,12 @@
+// export class VirusStat {
+//     constructor(country, cases, deaths, tests, day, time) {
+//         this.country = country;
+//         this.cases = cases;
+//         this.deaths = deaths;
+//         this.tests = tests;
+//         this.day = day;
+//         this.time = time;
+//     }
+// };
+
+
